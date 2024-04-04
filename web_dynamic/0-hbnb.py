3fdc6d6ace01aa30dd89e61f4a366b890ab06bbc
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2024-04-04 17:22:27 +0000

    updated task 1 - web dynamic dir, and html file

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Starts a Flask Web Application """
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,18 +7,18 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-import uuid
-
 app = Flask(__name__)
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
+
 
 @app.teardown_appcontext
 def close_db(error):
     """ Remove the current SQLAlchemy Session """
     storage.close()
 
-@app.route('/0-hbnb/', strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -34,13 +34,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    cache_id = str(uuid.uuid4())  # Generate a UUID for cache busting
-
-    return render_template('0-hbnb.html',
+    return render_template('100-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places,
-                           cache_id=cache_id)  # Pass cache_id to template
+                           places=places)
+
 
 if __name__ == "__main__":
     """ Main Function """
